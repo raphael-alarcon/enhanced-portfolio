132b08021a8efd133c90b27284986019cbd56998
Raphaël ALARCON <raphael-alarcon>
2023-10-11 15:58:49 +0200

    Base commit

---
+++ src/config/site.ts
@@ -0,0 +1,15 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+    title: "Portfolio",
+    description: "My open source portfolio crafted with NextJS, Shadcn/UI, and Bun.",
+    author: "Raphael ALARCON",
+    url: "",
+    email: "raph.alarcon1@gmail.com",
+    logo: "",
+    links: {
+        twitter: "https://twitter.com/SalutCDensetsu",
+        github: "https://github.com/raphael-alarcon",
+        linkedin: "https://www.linkedin.com/in/raphael-alarcon/",
+    },
+};
---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export type SiteConfig = {
+    title: string;
+    description: string;
+    author: string;
+    url: string;
+    email: string;
+    logo: string;
+    links: {
+        twitter: string;
+        github: string;
+        linkedin: string;
+    };
+};
