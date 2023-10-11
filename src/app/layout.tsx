78885574632ea9bd3b9f5f9fe435e224e11ac125
raphael-alarcon <raph.alarcon1@gmail.com>
2023-10-11 17:24:28 +0200

    feat(navigation): :lipstick: added navbar elements and their routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { siteConfig } from "@/config/site";
+import { navConfig, siteConfig } from "@/config/site";
+import { Navigation } from "@/components/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,7 +26,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
     return (
         <html lang="en">
             <body className={inter.className}>
-                {children}</body>
+                <Navigation items={navConfig.items}/>
+                {children}
+            </body>
         </html>
     );
 }
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,4 +1,4 @@
-import { SiteConfig } from "@/types";
+import { NavConfig, SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
     title: "Portfolio",
@@ -12,4 +12,25 @@ export const siteConfig: SiteConfig = {
         github: "https://github.com/raphael-alarcon",
         linkedin: "https://www.linkedin.com/in/raphael-alarcon/",
     },
+};
+
+export const navConfig: NavConfig = {
+    items: [
+        {
+            title: "Home",
+            href: "/",
+        },
+        {
+            title: "About",
+            href: "/about",
+        },
+        {
+            title: "Projects",
+            href: "/projects",
+        },
+        {
+            title: "Contact",
+            href: "/contact",
+        },
+    ],
 };
