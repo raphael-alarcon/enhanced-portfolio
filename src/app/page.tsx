e3b7a7348945c2f9bb6b6a1222969c8dfb5eff5e
Raphael <raphael.alarcon@etu.u-bordeaux.fr>
2024-02-29 12:43:19 +0100

    fix(SSR): fix hook useMediaQuery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,26 +3,26 @@ import { HomeSection } from "@/components/sections/home-section";
 
 type Section = {
 	key: string;
-	Component: React.ReactNode;
+	component: React.ReactNode;
 };
 
 const sections: Section[] = [
 	{
 		key: "home",
-		Component: <HomeSection/>,
+		component: <HomeSection/>,
 	},
 	{
 		key: "about",
-		Component: <AboutSection/>,
+		component: <AboutSection/>,
 	},
 ];
 
 export default function IndexPage() {
 	return (
 		<main>
-			{sections.map(({ key, Component }) => (
+			{sections.map(({ key, component }) => (
 				<section key={key} className="px-10 sm:px-40 flex flex-col justify-around min-h-screen items-center snap-start">
-					{Component}
+					{component}
 				</section>
 			))}
 		</main>
